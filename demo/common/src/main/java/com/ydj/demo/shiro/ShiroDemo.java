2506ef1938bdbb6dd0ca8a3a5028c8c1757112df
ydj <Carlos.yi@stu.hutb.edu.com>
2020-04-15 17:32:41 +0800

    说明：添加注释

--- demo/common/src/main/java/com/ydj/demo/shiro/ShiroDemo.java
+++ demo/common/src/main/java/com/ydj/demo/shiro/ShiroDemo.java
@@ -28,25 +28,41 @@ public class ShiroDemo {
 
     @Before
     public void addUser() {
+        // 添加用户的时候为此用户添加角色，一个用户可以拥有一个或者多个角色
         simpleAccountRealm.addAccount("ydj", "123456", "admin");
     }
 
     @Test
     public void shiroTest() {
+        /**
+         * 1.构建Security Manager环境
+         * Security Manager是用来提供安全服务，所以在做shiro认证的时候要先创建此对象
+         * 创建Security Manager对象之后要设置Realm
+         */
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
         securityManager.setRealm(simpleAccountRealm);
+        /**
+         * 2.获取向Security Manager提交请求的subject
+         * 而主体subject可以通过shiro提供的一个工具类SecurityUtiles来获取
+         */
         SecurityUtils.setSecurityManager(securityManager);
         Subject subject = SecurityUtils.getSubject();
+        /**
+         * 3.主体subject提交请求给Security Manager  -- > subject.login(token)
+         */
+        //提交请求时需要一个token，所以先创建token
         UsernamePasswordToken token = new UsernamePasswordToken("ydj", "123456");
         subject.login(token);
+        /**
+         * 4.shiro提供一个检查主体subject是否认证的方法isAuthenticated(),
+         *  此方法的返回结果是一个boolean值
+         */
         System.out.println("是否登录：" + subject.isAuthenticated());
+        // 校验角色
         subject.checkRoles("admin");
 
         // 退出登录
         subject.logout();
         System.out.println("是否登录：" + subject.isAuthenticated());
-
-
-
     }
 }
